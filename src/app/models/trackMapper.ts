ddd910710423eb712698b06e4267b81a2d356679
Karine Bauch <karine.bauch.web@gmail.com>
2022-10-13 09:01:49 +0200

    change the SQL request of trackMapper

--- src/app/models/trackMapper.ts
+++ src/app/models/trackMapper.ts
@@ -4,13 +4,30 @@ import type { Track } from '../../../types/track';
 export class TrackService {
   
   async findAllTracks() : Promise<Track[]> {
-    const result = await client.query('SELECT * FROM "track"');
+    const result = await client.query(
+      `SELECT "track"."id" AS "track_id",
+      "track"."name" AS "track_name",
+      "country",
+      "city",
+      array_agg(DISTINCT("race"."name")) AS "track_races"
+      FROM "track"
+      FULL JOIN "race" ON "track"."id" = "track_id"
+      GROUP BY "track"."id", "track"."name", "country"`
+    );
     return result.rows;
   }
 
   async findOneTrack(trackId: number) : Promise<Track> {
       const result = await client.query(
-        'SELECT * FROM "track" WHERE "track"."id" = $1',
+        `SELECT "track"."id" AS "track_id",
+        "track"."name" AS "track_name",
+        "country",
+        "city",
+        array_agg(DISTINCT("race"."name")) AS "track_races"
+        FROM "track"
+        FULL JOIN "race" ON "track"."id" = "track_id"
+        WHERE "track"."id" = $1
+        GROUP BY "track"."id", "track"."name", "country"`,
         [trackId]
       );
       return result.rows[0];
